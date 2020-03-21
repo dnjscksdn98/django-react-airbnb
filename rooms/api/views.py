90a006b872429f12e371be53eff4def33fef789c
Alex <dnjscksdn98@gmail.com>
2020-03-21 18:50:21 +0900

    #2.4 add search & ordering fields

--- rooms/api/views.py
+++ rooms/api/views.py
@@ -1,13 +1,18 @@
 from rest_framework import generics
 from rest_framework import permissions
 from rest_framework import status
+from rest_framework import filters
 
 from rooms import models as room_models
 from . import serializers as room_serializers
 
 
-class RoomView(generics.ListCreateAPIView):
+class RoomView(generics.ListAPIView):
 
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [permissions.AllowAny]
     serializer_class = room_serializers.RoomSerializer
     queryset = room_models.Room.objects.all()
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['name', 'description', 'country',
+                     'city', 'address', 'room_type__name']
+    ordering_fields = ['-total_rating']
