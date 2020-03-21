d98a3bdfb263eb0264f704c67c9095ab485c7286
Alex <dnjscksdn98@gmail.com>
2020-03-21 19:02:28 +0900

    #2.5 add pagination

--- rooms/api/views.py
+++ rooms/api/views.py
@@ -2,6 +2,7 @@ from rest_framework import generics
 from rest_framework import permissions
 from rest_framework import status
 from rest_framework import filters
+from rest_framework import pagination
 
 from rooms import models as room_models
 from . import serializers as room_serializers
@@ -11,7 +12,9 @@ class RoomView(generics.ListAPIView):
 
     permission_classes = [permissions.AllowAny]
     serializer_class = room_serializers.RoomSerializer
+    pagination_class = pagination.PageNumberPagination
     queryset = room_models.Room.objects.all()
+
     filter_backends = [filters.SearchFilter, filters.OrderingFilter]
     search_fields = ['name', 'description', 'country',
                      'city', 'address', 'room_type__name']
